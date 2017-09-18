0a1d3ec8a0593996993531eb0575f916c272ed0e
Daniel Doppelt <daniel.doppelt@mac.com>
2017-09-18 06:33:14 +0300

    validation

--- src/AppendableBufferLinkedList.java
+++ src/AppendableBufferLinkedList.java
@@ -1,45 +1,37 @@
-
 import java.util.LinkedList;
 
 public class AppendableBufferLinkedList extends LinkedList<BufferNode> implements IAppendableBufferList {
 
     private int contentLength;
 
-    private AppendableBufferLinkedList(){
+    private AppendableBufferLinkedList() {
     }
 
-    public static IAppendableBufferList create(){
+    public static IAppendableBufferList create() {
         return new AppendableBufferLinkedList();
     }
 
     @Override
     public boolean add(BufferNode node) {
-        if (node.getContentLength() > 0) {
-            this.contentLength += node.getContentLength();
-            return super.add(node);
-        }
-        return false;
+        this.contentLength += node.getContentLength();
+        return super.add(node);
     }
 
     @Override
     public boolean add(char[] node, int contentLength) {
-        if (contentLength > 0) {
-            //TODO: throw exception in case of an empty node
-            this.contentLength += contentLength;
-            return super.add(new BufferNode(node, contentLength));
-        }
-        return false;
+        BufferNode bufferNode = new BufferNode(node, contentLength);
+        return this.add(bufferNode);
     }
 
     @Override
     public char[] join() {
-        char[] bf = new char[contentLength];
-        int cur = 0;
+        char[] buffer = new char[contentLength];
+        int    cursor = 0;
         for (BufferNode node : this) {
-            System.arraycopy(node.content, 0, bf, cur, node.contentLength);
-            cur += node.contentLength;
+            System.arraycopy(node.getContent(), 0, buffer, cursor, node.getContentLength());
+            cursor += node.getContentLength();
         }
-        return bf;
+        return buffer;
     }
 
 }
--- src/BufferNode.java
+++ src/BufferNode.java
@@ -1,10 +1,10 @@
 public class BufferNode {
 
-    final char[] content;
-    final int contentLength;
+    private final char[] content;
+    private final int    contentLength;
 
     public BufferNode(char[] content, int contentLength) {
-        // TODO: ensure byte[] size matches length
+        validate(content, contentLength);
         this.content = content;
         this.contentLength = contentLength;
     }
@@ -15,5 +15,14 @@ public class BufferNode {
 
     public int getContentLength() {
         return contentLength;
+    }
+
+    private void validate(char[] content, int contentLength) {
+        if (content.length != contentLength) {
+            throw new IllegalStateException("actual node content length doesn't match declared content length");
+        }
+        if (contentLength > 0) {
+            throw new IllegalStateException("content length should not be 0");
+        }
     }
 }
